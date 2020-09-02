5e0e42c154e2c634bfdc2bc9ea14d8b537a76471
MarcoCiaramella <marcociaramella87@gmail.com>
2020-09-02 15:19:29 +0200

    added image left right

--- voxelize.py
+++ voxelize.py
@@ -4,15 +4,16 @@ from PIL import Image
 
 
 img_front = sys.argv[1]
-img_side = sys.argv[2]
-img_top = sys.argv[3]
-img_bottom = sys.argv[4]
-img_back = sys.argv[5]
+img_right = sys.argv[2]
+img_left = sys.argv[3]
+img_top = sys.argv[4]
+img_bottom = sys.argv[5]
+img_back = sys.argv[6]
 
 
 def getSize(img_front,img_side):
     with Image.open(img_front) as image:
         x,y = image.size
-    with Image.open(img_side) as image:
+    with Image.open(img_right) as image:
         z,y = image.size
     return x,y,z
